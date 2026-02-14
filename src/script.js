a1cb3f0a6e6b767717168e55410757aad975fdd8
Yashvardhan Thorat <thoratyashvardhan2008@gmail.com>
2026-02-15 01:34:46 +0530

    Adiing the script

---
+++ src/script.js
@@ -0,0 +1,21 @@
+import { loginWithGoogle } from "./firebase.js";
+
+document.getElementById("loginBtn").addEventListener("click", async () => {
+  try {
+    const user = await loginWithGoogle();
+
+    console.log("Logged in:", user.displayName);
+
+    // Save user in localStorage (so next pages can access)
+    localStorage.setItem("userName", user.displayName);
+    localStorage.setItem("userEmail", user.email);
+    localStorage.setItem("userUID", user.uid);
+
+    // Redirect to form page
+    window.location.href = "form.html";
+
+  } catch (error) {
+    console.error(error);
+    alert("Login failed");
+  }
+});
