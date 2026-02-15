38cd5a9bf1eb14b51dceab4b6195d5f7ba63f76f
Prashant <Prashantsengar25031@gmail.com>
2026-02-15 07:15:12 +0530

    asd

--- src/script.js
+++ src/script.js
@@ -11,16 +11,9 @@ document.getElementById("loginBtn").addEventListener("click", async () => {
 
   console.log("Logged in:", user.displayName);
 
-    // Save user in localStorage (so next pages can access)
-    localStorage.setItem("userName", user.displayName);
-    localStorage.setItem("userEmail", user.email);
-    localStorage.setItem("userUID", user.uid);
+  localStorage.setItem("userName", user.displayName);
+  localStorage.setItem("userEmail", user.email);
+  localStorage.setItem("userUID", user.uid);
 
-    // Redirect to form page
-    window.location.href = "home_page.html";
-
-  } catch (error) {
-    console.error(error);
-    alert("Login failed");
-  }
+  window.location.href = "home_page.html";
 });
